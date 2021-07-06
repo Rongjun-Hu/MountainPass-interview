476f2e8136d0720eabf223d6574d0ae9764109f5
Peter <rongjun.hu.0716@gmail.com>
2021-07-06 11:45:47 +1000

    Changed props name.

--- src/sections/QuickActions/index.js
+++ src/sections/QuickActions/index.js
@@ -6,8 +6,16 @@ const QuickActions = () => {
     <div className="quickActions">
       <h1>Quick Actions</h1>
       <div className="quickActions__buttonContainer">
-        <ButtonBox description="Add Service" isDashed={true} />
-        <ButtonBox description="Deploy Gatsby Website" isDashed={true} />
+        <ButtonBox
+          description="Add Service"
+          hasStyle={true}
+          solid="buttonBox--dashed"
+        />
+        <ButtonBox
+          description="Deploy Gatsby Website"
+          hasStyle={true}
+          solid="buttonBox--dashed"
+        />
         <ButtonBox description="Deploy Node Project" />
       </div>
     </div>
