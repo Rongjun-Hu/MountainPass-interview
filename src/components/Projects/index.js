a52463c28111f114bc91e118464e086bc2c74d1f
Peter <rongjun.hu.0716@gmail.com>
2021-07-06 13:31:40 +1000

    Make Services section as parent, pass fetched data to child.

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,27 +1,7 @@
-import { useState, useEffect } from "react";
 import Project from "./Project";
-import axios from "axios";
 import "./Projects.css";
-import ButtonBox from "../ButtonBox";
 
-const Projects = () => {
-  const [projects, setProjects] = useState([]);
-
-  const getAllProjects = async () => {
-    const { data } = await axios.get(process.env.REACT_APP_BASE_URL, {
-      headers: {
-        Authorization: `Basic ${process.env.REACT_APP_AUTH}`,
-      },
-    });
-
-    setProjects(data.data);
-    console.log(data);
-  };
-
-  useEffect(() => {
-    getAllProjects();
-  }, []);
-
+const Projects = ({ projects }) => {
   return (
     <div className="projects">
       <Project projects={projects} />
--- src/sections/Services/index.js
+++ src/sections/Services/index.js
@@ -1,18 +1,48 @@
+import { useEffect, useState } from "react";
 import Buttons from "../../components/Buttons";
+import Modal from "../../components/Modal";
 import Projects from "../../components/Projects";
+import { Pagination } from "@material-ui/lab";
+import axios from "axios";
 import "./services.css";
 
 const Services = () => {
+  const [openModal, setOpenModal] = useState(false);
+  const [projects, setProjects] = useState([]);
+
+  const getAllProjects = async () => {
+    const { data } = await axios.get(process.env.REACT_APP_BASE_URL, {
+      headers: {
+        Authorization: `Basic ${process.env.REACT_APP_AUTH}`,
+      },
+    });
+
+    setProjects(data.data);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getAllProjects();
+  }, []);
+
   return (
     <div className="services">
       <h1>Services</h1>
       <div className="services__input">
         <input type="text" placeholder="Filter" />
-        <Buttons name="ADD SERVICE" />
+        <Buttons name="ADD SERVICE" showModal={() => setOpenModal(true)} />
       </div>
       <div className="services__projectList">
-        <Projects />
+        <Projects projects={projects} />
       </div>
+      <br />
+      <Pagination style={{ margin: "0 auto" }} />
+      {openModal && (
+        <Modal
+          getAllProjects={getAllProjects}
+          closeModal={() => setOpenModal(false)}
+        />
+      )}
     </div>
   );
 };
