8ffc617d0ca10297c91d4e5a9d3ffc63940cd24a
Peter <rongjun.hu.0716@gmail.com>
2021-07-06 11:44:39 +1000

    Using static button, not ButtonBox component.

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import Project from "./Project";
 import axios from "axios";
 import "./Projects.css";
+import ButtonBox from "../ButtonBox";
 
 const Projects = () => {
   const [projects, setProjects] = useState([]);
@@ -24,6 +25,9 @@ const Projects = () => {
   return (
     <div className="projects">
       <Project projects={projects} />
+      <div className="projects__button">
+        <button>Add Service</button>
+      </div>
     </div>
   );
 };
