87717765877f58aea994512208f09cb25b34e2a7
Peter <rongjun.hu.0716@gmail.com>
2021-07-07 19:46:42 +1000

    Using new props.

--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -1,8 +1,8 @@
 import "./Buttons.css";
 
-const Buttons = ({ name, showModal }) => {
+const Buttons = ({ name, type, handleClick, style }) => {
   return (
-    <button className="buttons" onClick={showModal}>
+    <button className="buttons" type={type} onClick={handleClick} style={style}>
       {name}
     </button>
   );
