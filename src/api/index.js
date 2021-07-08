b5c600aeec0bf20df875e49b7a27fd729e3fbba3
Peter Hu <rongjun.hu.0716@gmail.com>
2021-07-08 12:10:47 +1000

    Remove basic auth.

--- src/api/index.js
+++ src/api/index.js
@@ -3,11 +3,9 @@ import axios from "axios";
 const API = axios.create({
   baseURL: "https://interview-web-service.mountainpass.com.au/api/v1",
   headers: {
-    Authorization: `Basic cGV0ZXI6QXFRSw==`,
+    Authorization: `Basic `,
   },
 });
-
-// const authToken = "cGV0ZXI6QXFRSw==";
 
 export const getAllProjects = (page) => API.get(`/projects?page=${page}`);
 export const createProject = (newProject) => API.post("/projects", newProject);
