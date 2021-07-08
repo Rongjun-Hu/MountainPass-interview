21234bbf1c616c2f86c9d63ffc352405c6031932
Peter Hu <rongjun.hu.0716@gmail.com>
2021-07-08 12:08:57 +1000

    API.

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://interview-web-service.mountainpass.com.au/api/v1/projects",
+  baseURL: "https://interview-web-service.mountainpass.com.au/api/v1",
   headers: {
-    Authorization: `Basic ${process.env.REACT_APP_AUTH}`,
+    Authorization: `Basic cGV0ZXI6QXFRSw==`,
   },
 });
 
 // const authToken = "cGV0ZXI6QXFRSw==";
 
-export const getAllProjects = (page) => API.get(`?page=${page}`);
-export const createProject = (newProject) => API.post("/", newProject);
+export const getAllProjects = (page) => API.get(`/projects?page=${page}`);
+export const createProject = (newProject) => API.post("/projects", newProject);
