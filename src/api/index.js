8cdc9866744c37db24488d7a3cc1cec55c0c5c14
Peter <rongjun.hu.0716@gmail.com>
2021-07-12 14:57:10 +1000

    Typo.

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
-    Authorization: `Basic ${process.send.REACT_APP_AUTH}`,
+    Authorization: `Basic ${process.env.REACT_APP_AUTH}`,
   },
 });
 
